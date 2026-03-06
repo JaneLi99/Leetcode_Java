265d021f3b8bb207c12deaa7a8a8388b815ce794
JaneLi99 <jane990423@gmail.com>
2026-03-06 18:04:24 -0500

    March 6

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -29,10 +29,61 @@ public class ThreeSum {
         return results;
     }
 
+    public List<List<Integer>> threeSumBetterWay(int[] nums) {
+        int target = 0;
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                if (nums[i] + nums[j] + nums[k] == target) {
+                    List<Integer> subResult = List.of(nums[i], nums[j], nums[k]);
+                    if (!result.contains(subResult)) {
+                        result.add(subResult);
+                    }
+                    j ++;
+                    k --;
+                } else if (nums[i] + nums[j] + nums[k] < target) {
+                    j ++;
+                } else {
+                    k --;
+                }
+            }
+        }
+        return result;
+    }
+
+    public List<List<Integer>> threeSumBestWay (int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         ThreeSum sol = new ThreeSum();
         int[] nums = {-1, 0, 1, 2, -1, -4};
-        System.out.println(sol.threeSum(nums)); // Expected: [[-1, -1, 2], [-1, 0, 1]]
+//        sorted list: {-4, -1, -1, 0, 1, 2}
+        System.out.println(sol.threeSumBestWay(nums)); // Expected: [[-1, -1, 2], [-1, 0, 1]]
     }
 
 }
